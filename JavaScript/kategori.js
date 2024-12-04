b4e9e527a376166df2a6c32dcfb1c9e0f12a91e6
ProofHarata <fazafaqihalawi@gmail.com>
2024-12-04 09:38:04 +0700

    menambahkan fitur pembelian

--- JavaScript/kategori.js
+++ JavaScript/kategori.js
@@ -248,3 +248,28 @@ const cart = document.querySelector('.shopping-cart-buy')
 document.querySelector('#shopping-cart').onclick = () => {
     cart.classList.toggle('active')
 }
+
+// FORM PEMBELIAN
+document.getElementById('btn-whatsapp').onclick = function(event) {
+    event.preventDefault(); // Mencegah form dari pengiriman default
+
+    // Mengambil nilai dari input
+    var name = document.getElementById('name').value;
+    var phone = document.getElementById('phone').value;
+    var email = document.getElementById('email').value; // Menambahkan email
+    var productId = document.getElementById('product-id').value; // Menambahkan ID produk
+    var address = document.getElementById('address').value;
+
+    // Membuat pesan untuk WhatsApp
+    var message = "Nama: " + name + 
+                "\nNomor Telepon: " + phone + 
+                "\nEmail: " + email + // Menambahkan email ke pesan
+                "\nID Produk: " + productId + // Menambahkan ID produk ke pesan
+                "\nAlamat: " + address;
+
+    // Membuat URL WhatsApp
+    var whatsappUrl = 'https://api.whatsapp.com/send?phone=6281413263843&text=' + encodeURIComponent(message);
+    
+    // Membuka URL WhatsApp di tab baru
+    window.open(whatsappUrl, '_blank');
+};
